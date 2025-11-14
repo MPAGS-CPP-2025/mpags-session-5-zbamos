c8e8fadb62b9c4c144eb5d02b6363757b5421e98
Zack Amos <zachariah.benjamin.amos@cern.ch>
2025-11-14 10:40:30 +0000

    Excercise 5.1

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -3,6 +3,56 @@
 #include <string>
 #include <vector>
 
+
+
+std::string transformChar( const char in_char)
+{
+    std::string out_string{};
+    // Uppercase alphabetic characters
+    if (std::isalpha(in_char)) {
+        // inputText += std::toupper(in_char);
+        out_string = std::toupper(in_char);
+        return out_string;
+    }
+
+    // Transliterate digits to English words
+    switch (in_char) {
+        case '0':
+            out_string += "ZERO";
+            break;
+        case '1':
+            out_string += "ONE";
+            break;
+        case '2':
+            out_string += "TWO";
+            break;
+        case '3':
+            out_string += "THREE";
+            break;
+        case '4':
+            out_string += "FOUR";
+            break;
+        case '5':
+            out_string += "FIVE";
+            break;
+        case '6':
+            out_string += "SIX";
+            break;
+        case '7':
+            out_string += "SEVEN";
+            break;
+        case '8':
+            out_string += "EIGHT";
+            break;
+        case '9':
+            out_string += "NINE";
+            break;
+    }
+
+    return out_string;
+}
+
+
 int main(int argc, char* argv[])
 {
     // Convert the command-line arguments into a more easily usable form
@@ -97,45 +147,8 @@ int main(int argc, char* argv[])
 
     // loop over each character from user input
     while (std::cin >> inputChar) {
-        // Uppercase alphabetic characters
-        if (std::isalpha(inputChar)) {
-            inputText += std::toupper(inputChar);
-            continue;
-        }
 
-        // Transliterate digits to English words
-        switch (inputChar) {
-            case '0':
-                inputText += "ZERO";
-                break;
-            case '1':
-                inputText += "ONE";
-                break;
-            case '2':
-                inputText += "TWO";
-                break;
-            case '3':
-                inputText += "THREE";
-                break;
-            case '4':
-                inputText += "FOUR";
-                break;
-            case '5':
-                inputText += "FIVE";
-                break;
-            case '6':
-                inputText += "SIX";
-                break;
-            case '7':
-                inputText += "SEVEN";
-                break;
-            case '8':
-                inputText += "EIGHT";
-                break;
-            case '9':
-                inputText += "NINE";
-                break;
-        }
+        inputText += transformChar(inputChar);
 
         // If the character isn't alphabetic or numeric, DONT add it
     }
